31a1675237c2ddf0c9e864b629151a76452591f7
codersuresh <sd807141@gmail.com>
2023-09-13 12:19:37 +0545

    refactor: format code

--- components/UpvoteDownvote.tsx
+++ components/UpvoteDownvote.tsx
@@ -7,13 +7,17 @@ const UpvoteDownvote = () => {
 
     return (
         <div className='flex sm:flex-col sm:py-3 px-3 sm:gap-2 gap-x-5  bg-very-light-gray w-fit h-fit rounded-xl text-center'>
-            <button onClick={() => setUpvote(prevUpvote => prevUpvote + 1)} className='text-light-grayish-blue hover:text-moderate-blue'>
+            <button
+                onClick={() => setUpvote(prevUpvote => prevUpvote + 1)}
+                className='text-light-grayish-blue hover:text-moderate-blue'>
                 <i className='fas fa-plus'></i>
             </button>
 
             <p className='text-moderate-blue font-medium my-3'>{upvote}</p>
 
-            <button onClick={() => setUpvote(prevUpvote => prevUpvote && prevUpvote - 1)} className='text-light-grayish-blue hover:text-moderate-blue'>
+            <button
+                onClick={() => setUpvote(prevUpvote => prevUpvote && prevUpvote - 1)}
+                className='text-light-grayish-blue hover:text-moderate-blue'>
                 <i className='fas fa-minus'></i>
             </button>
         </div>
