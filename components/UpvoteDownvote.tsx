f432a91ef628693956d5e3a40e62d4b692f154db
codersuresh <sd807141@gmail.com>
2023-09-13 12:18:17 +0545

    upvote and downvote using local state for now

--- components/UpvoteDownvote.tsx
+++ components/UpvoteDownvote.tsx
@@ -1,15 +1,19 @@
+'use client'
 import React from 'react'
 
 const UpvoteDownvote = () => {
+
+    const [upvote, setUpvote] = React.useState(0)
+
     return (
         <div className='flex sm:flex-col sm:py-3 px-3 sm:gap-2 gap-x-5  bg-very-light-gray w-fit h-fit rounded-xl text-center'>
-            <button className='text-light-grayish-blue hover:text-moderate-blue'>
+            <button onClick={() => setUpvote(prevUpvote => prevUpvote + 1)} className='text-light-grayish-blue hover:text-moderate-blue'>
                 <i className='fas fa-plus'></i>
             </button>
 
-            <p className='text-moderate-blue font-medium my-3'>22</p>
+            <p className='text-moderate-blue font-medium my-3'>{upvote}</p>
 
-            <button className='text-light-grayish-blue hover:text-moderate-blue'>
+            <button onClick={() => setUpvote(prevUpvote => prevUpvote && prevUpvote - 1)} className='text-light-grayish-blue hover:text-moderate-blue'>
                 <i className='fas fa-minus'></i>
             </button>
         </div>
