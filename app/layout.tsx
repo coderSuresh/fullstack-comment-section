888470ac8fc806cba7ba1f72664d924e7c65ed6c
codersuresh <sd807141@gmail.com>
2023-09-13 11:47:07 +0545

    add fontawesome cdn

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ const RootLayout = ({
 }: ChildrenProps) => {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+      </head>
       <body>{children}</body>
     </html>
   )
