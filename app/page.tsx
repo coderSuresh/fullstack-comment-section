1dcc074970d8806696066da076c7d4314b24ef0e
codersuresh <sd807141@gmail.com>
2023-09-13 08:08:49 +0545

    change home to rafc

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Home() {
+const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
     </main>
   )
 }
+
+export default Home
