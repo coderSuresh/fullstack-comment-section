b1ee1e91167eb9528ab64b6900b3993d5a3da767
codersuresh <sd807141@gmail.com>
2023-09-14 13:38:49 +0545

    feat: add reply design

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,12 @@ const Home = () => {
   return (
     <main className='md:w-[740px] md:mx-auto mx-5 my-10'>
       <CommentCard />
+      <CommentCard />
+      {/* replies */}
+      <div className='border-l sm:pl-10 pl-5 sm:ml-10 '>
+        <CommentCard />
+        <CommentCard />
+      </div>
     </main>
   )
 }
