8dce8141e316b1fe4d2db57f141f01fef8d58aa8
codersuresh <sd807141@gmail.com>
2023-09-17 13:38:17 +0545

    remove email from model for now

--- models/auth/Register.ts
+++ models/auth/Register.ts
@@ -6,12 +6,6 @@ const RegisterSchema = new Schema({
         required: [true, 'Please provide a name'],
     },
 
-    email: {
-        type: String,
-        required: [true, 'Please provide an email'],
-        unique: true,
-    },
-
     password: {
         type: String,
         required: [true, 'Please provide a password'],
