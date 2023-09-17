b6d799017e69430ba176d09e594cc5b26b3d927a
codersuresh <sd807141@gmail.com>
2023-09-17 15:21:42 +0545

    add database name

--- utils/database.js
+++ utils/database.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            dbName: 'full-stack-comments',
         })
     } catch (error) {
         console.error(`Error: ${error.message}`)
