2725844ae23eace7563e6fd03df54caf32678349
codersuresh <sd807141@gmail.com>
2023-09-17 22:45:55 +0545

    feat: login user

---
+++ app/api/login/route.ts
@@ -0,0 +1,43 @@
+import { connectDB } from "@/utils/database"
+import Register from "@/models/auth/Register"
+import bcrypt from "bcrypt"
+
+const POST = async (req: Request) => {
+    try {
+        const { username, password } = await req.json()
+
+        await connectDB()
+
+        const dbUserData = await Register.findOne({
+            'username': username
+        })
+
+        if (!username || !password) return new Response(JSON.stringify({ error: 'All fields are required' }), {
+            headers: { 'Content-Type': 'application/json' },
+        })
+
+        if (!dbUserData) return new Response(JSON.stringify({ error: 'Username does not exist' }), {
+            headers: { 'Content-Type': 'application/json' },
+        })
+
+        const isPasswordCorrect = await bcrypt.compare(password, dbUserData.password)
+
+        if (!isPasswordCorrect) return new Response(JSON.stringify({ error: 'Password is incorrect' }), {
+            headers: { 'Content-Type': 'application/json' },
+        })
+
+        return new Response(JSON.stringify({ message: 'User logged in' }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        })
+
+    } catch (error) {
+        return new Response(JSON.stringify({ message: "hello" + error }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        })
+    }
+
+}
+
+export { POST }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,65 @@
 'use client'
 import Link from 'next/link'
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 const Login = () => {
 
     const [showPassword, setShowPassword] = React.useState(false)
+    const [loading, setLoading] = React.useState(false)
+    const [formData, setFormData] = React.useState({
+        username: '',
+        password: ''
+    })
+    const [error, setError] = React.useState('')
+
+    const router = useRouter()
+
+    const loginUser = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if (e.currentTarget.checkValidity()) {
+            setLoading(true)
+            fetch('/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.error) {
+                        setError(data.error)
+                    }
+                    else {
+                        router.push('/')
+                    }
+                    setLoading(false)
+                })
+        }
+        else {
+            e.currentTarget.reportValidity()
+        }
+    }
+
+    React.useEffect(() => {
+        setError('')
+    }, [formData])
 
     return (
         <>
             <h1 className='text-2xl font-semibold mb-5 text-center'>Login</h1>
 
-            <form className='flex flex-col'>
+            <form onSubmit={(e) => loginUser(e)} className='flex flex-col'>
+
+                {error && <p className='mb-5 text-soft-red text-sm text-center'>{error}</p>}
 
                 <label className='mb-2'>Username</label>
                 <input
+                    required
+                    onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+                    value={formData.username}
                     className='mb-5 p-2 border focus:outline-none focus:border-moderate-blue rounded-md'
                     type='text' placeholder='johndoe'
                 />
@@ -21,12 +67,15 @@ const Login = () => {
                 <label className='mb-2'>Password</label>
                 <div className='relative'>
                     <input
+                        required
+                        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                        value={formData.password}
                         className='mb-5 p-2 w-full border focus:outline-none focus:border-moderate-blue rounded-md'
                         type={`${showPassword ? 'text' : 'password'}`} placeholder='xxxxxxxx'
                     />
-                    <button type='button' onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                    <button type='button' tabIndex={-1} onClick={() => setShowPassword((showPassword) => !showPassword)}>
                         <i className={`fas fa-eye${showPassword ? '-slash' : ''} absolute right-2 top-3`} />
-                   </button>
+                    </button>
                 </div>
 
                 <p className='mb-5 text-right text-sm'>
@@ -34,7 +83,7 @@ const Login = () => {
                 </p>
 
                 <button className='bg-moderate-blue outline focus:outline-4 hover:outline-outline-color focus:outline-outline-color uppercase font-medium text-white py-2 rounded-md'>
-                    Login
+                    {loading ? <i className='fas fa-spinner fa-spin' /> : 'Login'}
                 </button>
 
                 <p className='mt-5 text-center'>
