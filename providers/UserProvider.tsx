ef149eee564cad1dcac292cfb4ff947517ff9589
codersuresh <sd807141@gmail.com>
2023-09-18 15:00:49 +0545

    feat: fetch user data from localstorage to context

--- providers/UserProvider.tsx
+++ providers/UserProvider.tsx
@@ -1,16 +1,20 @@
 'use client'
-import {UserContext} from '@/context/UserContext'
+import { UserContext } from '@/context/UserContext'
 import { UserContextType } from '@/types/UserContextTypes'
 import { ChildrenProps } from '@/types/props'
 import React from 'react'
 
 const UserProvider = ({ children }: ChildrenProps) => {
 
-  const [values, setValues] = React.useState<UserContextType>({
-    name: '',
-    username: '',
-    isLoggedIn: false,
-  })
+  const storedUser = JSON.parse(localStorage.getItem('user') || '{}')
+
+  const [values, setValues] = React.useState<UserContextType>(
+    {
+      name: storedUser.name || '',
+      username: storedUser.username || '',
+      isLoggedIn: storedUser.isLoggedIn || false,
+    }
+  )
 
   return (
     <UserContext.Provider value={{ values, setValues }}>
