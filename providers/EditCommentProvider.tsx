0b52ba1c6d4094120eeb5c4903da78128ba0c39b
codersuresh <sd807141@gmail.com>
2023-10-07 13:36:27 +0545

    fix: add missing properties

--- providers/EditCommentProvider.tsx
+++ providers/EditCommentProvider.tsx
@@ -8,7 +8,10 @@ const EditCommentProvider = ({ children }: ChildrenProps) => {
 
     const [editCommentValues, setEditCommentValues] = React.useState<EditCommentContextType>({
         editComment: false,
+        editedComment: '',
         id: '',
+        isReply: false,
+        commentId: '',
     })
 
     return (
